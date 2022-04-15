08b23ab1992b39eaab5f3e4144f7ec88530497f9
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2022-04-15 22:34:10 +0700

    Redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CaraPindah } from './CaraPindah';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CaraPindah/>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
